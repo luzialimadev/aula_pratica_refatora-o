b5f0fa942e53ded34fbd1711b3036ec80a5b13d5
Luzialimadev <luzia.008lima@gmail.com>
2022-12-14 23:54:49 -0300

    commit 1

---
+++ src/Statement.java
@@ -0,0 +1,9 @@
+import java.util.Enumeration;
+
+public abstract class Statement {
+    public abstract String statement(Enumeration<Rental> rentals, Customer customer);
+
+    public String value(Customer aCustomer) {
+        return this.statement(aCustomer.getRentals(), aCustomer);
+    }
+}
